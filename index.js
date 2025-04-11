1bbc3cd24a61033e58f5bfde34fbe3d52172915b
danielvasic <dvasic1@gmail.com>
2025-04-11 10:26:58 +0200

    chore: initialize package.json for Smart Campus application with express dependency

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Express!');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
