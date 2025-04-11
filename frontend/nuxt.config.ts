986271b75b4da0d754f9bdcd9aaec076dc3777a7
danielvasic <dvasic1@gmail.com>
2025-04-11 10:22:54 +0200

    feat: initialize frontend with Nuxt 3 and Tailwind CSS
    - Added package.json with dependencies for Nuxt, Tailwind CSS, and related libraries.
    - Included favicon.ico for branding.
    - Created an empty robots.txt file for SEO purposes.
    - Set up TypeScript configuration for server and frontend.

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,19 @@
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  }
+})
